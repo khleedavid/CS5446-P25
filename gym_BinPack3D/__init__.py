c31a38d6eac69b2faf5f6cf2b82909b6a69b752d
Lee Young Ho <lkxv999@gmail.com>
2024-04-22 19:54:50 +0800

    training code uploaded.

---
+++ gym_BinPack3D/__init__.py
@@ -0,0 +1,7 @@
+# from gym.envs.registration import register
+from gymnasium.envs.registration import register
+
+register(
+    id='BinPack3D-v0',
+    entry_point='gym_BinPack3D.envs:PackingGame',
+)
